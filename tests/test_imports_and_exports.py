a453280eacae6e8adfc9513ffc03081e8cbd67af
Alys Clark <alys.clark@auckland.ac.nz>
2018-03-14 12:51:18 +1300

    Correcting testdata filename

--- tests/test_imports_and_exports.py
+++ tests/test_imports_and_exports.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
-
+import os
 import placentagen
+
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'TestData/Small.exnode')
 
 class Test_import_exnode_tree(TestCase):
     def test_num_nodes(self):
-        nodedata=placentagen.import_exnode_tree('TestData/Small.exnode')
+        nodedata=placentagen.import_exnode_tree(TESTDATA_FILENAME)
         self.assertTrue(nodedata['total_nodes'] is 4)
