d94c0d6972c5b6fba29ec9338572eeb9a07db3db
Alys Clark <alys.clark@auckland.ac.nz>
2018-03-14 12:41:46 +1300

    Improving tests

--- tests/test_imports_and_exports.py
+++ tests/test_imports_and_exports.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import placentagen
 
-class Test_import_exnode_tree:
+class Test_import_exnode_tree(TestCase):
     def test_num_nodes(self):
         nodedata=placentagen.import_exnode_tree('TestData/Small.exnode')
         self.assertTrue(nodedata['total_nodes'] is 4)
