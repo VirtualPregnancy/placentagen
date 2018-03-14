f73afa4b2d0448083447b962f2bce4b833b3ea07
Alys Clark <alys.clark@auckland.ac.nz>
2018-03-14 13:31:17 +1300

    Adding new line to conform with PEP8.

--- tests/test_imports_and_exports.py
+++ tests/test_imports_and_exports.py
@@ -5,6 +5,7 @@ import numpy as np
 
 TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'TestData/Small.exnode')
 
+
 class Test_import_exnode_tree(TestCase):
     def test_num_nodes(self):
         nodedata=placentagen.import_exnode_tree(TESTDATA_FILENAME)
