bf5c591c6b2402d297c8cbc6b5e42ba5a8273a71
Alys Clark <alys.clark@auckland.ac.nz>
2018-03-23 14:37:07 +1300

    New function to generate an 'umbilical cord' to feed a generic placenta (human specific)

--- source/placentagen/generate_shapes.py
+++ source/placentagen/generate_shapes.py
@@ -8,11 +8,11 @@ def equispaced_data_in_ellipsoid(n, volume, thickness, ellipticity):
     # volume=volume of ellipsoid
     # thickness = placental thickness (z-dimension)
     # ellipticity = ratio of y to x axis dimensions
-    pi = np.pi
-    data_spacing = (volume / n) ** (1. / 3)
-    z_radius = thickness / 2.0
-    x_radius = np.sqrt(volume * 3.0 / (4.0 * pi * ellipticity * z_radius))
-    y_radius = ellipticity * x_radius
+    data_spacing = (volume / n) ** (1.0 / 3.0)
+    radii = calculate_ellipse_radii(volume, thickness, ellipticity)
+    z_radius = radii['z_radius']
+    x_radius = radii['x_radius']
+    y_radius = radii['y_radius']
 
     # Aiming to generate seed points that fill a cuboid encompasing the placental volume then remove seed points that are
     # external to the ellipsoid
@@ -39,7 +39,7 @@ def equispaced_data_in_ellipsoid(n, volume, thickness, ellipticity):
     count = 0
     for i in range(len(data_coords)):  # Loop through grid
         coord_check = (data_coords[i][0] / x_radius) ** 2 + (data_coords[i][1] / y_radius) ** 2 + (
-                    data_coords[i][2] / z_radius) ** 2  # check the point is in an ellipsoid
+                data_coords[i][2] / z_radius) ** 2  # check the point is in an ellipsoid
         if (coord_check) < 1.0:  # Has to be strictly in the ellipsoid
             Edata[count, :] = data_coords[i, :]  # add to data array
             count = count + 1
@@ -48,3 +48,63 @@ def equispaced_data_in_ellipsoid(n, volume, thickness, ellipticity):
     print('Data points within ellipsoid allocated. Total = ' + str(len(Edata)))
 
     return Edata
+
+
+def umbilical_seed_geometry(volume, thickness, ellipticity, insertion_x, insertion_y, umb_artery_distance):
+    radii = calculate_ellipse_radii(volume, thickness, ellipticity)
+    z_radius = radii['z_radius']
+    x_radius = radii['x_radius']
+    y_radius = radii['y_radius']
+    node_loc = np.zeros((6, 4))
+    elems = np.zeros((5, 3))
+    node_loc[0][0] = 1
+    node_loc[0][1] = insertion_x
+    node_loc[0][2] = insertion_y
+    node_loc[0][3] = z_radius * np.sqrt(
+        1.0 - (node_loc[0][0] / x_radius) ** 2 - (node_loc[0][1] / y_radius) ** 2) + 23.0
+
+    # node 2 is 3 mm up from node 1 in the z direction
+    node_loc[1][0] = 2
+    node_loc[1][1] = insertion_x
+    node_loc[1][2] = insertion_y
+    node_loc[1][3] = z_radius * np.sqrt(
+        1.0 - (node_loc[0][0] / x_radius ** 2) - (node_loc[0][1] / y_radius) ** 2) + 20.0
+
+    # node 3 & 4 is the start of the 'umbilical artery'
+    node_loc[2][0] = 3
+    node_loc[2][1] = insertion_x
+    node_loc[2][2] = insertion_y - umb_artery_distance / 2.0
+    node_loc[2][3] = z_radius * np.sqrt(
+        1.0 - (node_loc[0][0] / x_radius) ** 2 - (node_loc[0][1] / y_radius) ** 2) + 20.0
+    node_loc[3][0] = 4
+    node_loc[3][1] = insertion_x
+    node_loc[3][2] = insertion_y + umb_artery_distance / 2.0
+    node_loc[3][3] = z_radius * np.sqrt(
+        1.0 - (node_loc[0][0] / x_radius) ** 2 - (node_loc[0][1] / y_radius) ** 2) + 20.0
+
+    # node 5 and 6 'hit' the chorionic plate.
+    node_loc[4][0] = 5
+    node_loc[4][1] = insertion_x
+    node_loc[4][2] = insertion_y - umb_artery_distance / 2.0
+    node_loc[4][3] = z_radius * np.sqrt(1.0 - (node_loc[4][0] / x_radius) ** 2 - (node_loc[4][1] / y_radius) ** 2)
+    node_loc[5][0] = 6
+    node_loc[5][1] = insertion_x
+    node_loc[5][2] = insertion_y + umb_artery_distance / 2.0
+    node_loc[5][3] = z_radius * np.sqrt(1.0 - (node_loc[5][0] / x_radius) ** 2 - (node_loc[5][1] / y_radius) ** 2)
+
+    elems[0, :] = [1, 1, 2]
+    elems[1, :] = [2, 2, 3]
+    elems[2, :] = [3, 2, 4]
+    elems[3, :] = [4, 3, 5]
+    elems[4, :] = [5, 4, 6]
+
+    return{'umb_nodes':node_loc, 'umb_elems':elems}
+
+
+def calculate_ellipse_radii(volume, thickness, ellipticity):
+    pi = np.pi
+    z_radius = thickness / 2.0
+    x_radius = np.sqrt(volume * 3.0 / (4.0 * pi * ellipticity * z_radius))
+    y_radius = ellipticity * x_radius
+
+    return {'x_radius': x_radius, 'y_radius': y_radius, 'z_radius': z_radius}
