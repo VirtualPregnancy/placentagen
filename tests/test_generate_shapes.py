35b77019f1fdbf1a311ed60bfd3ac5b0f578dfa4
Alys Clark <alys.clark@auckland.ac.nz>
2018-03-23 16:46:55 +1300

    New test for generating data points on an ellipsoid

--- tests/test_generate_shapes.py
+++ tests/test_generate_shapes.py
@@ -12,6 +12,12 @@ class Test_generate_data(TestCase):
         array_test = np.isclose(datapoints, [0.0, 0.0, 0.0])
         self.assertTrue(array_test.all)
 
+    def test_data_on_ellipsoid(self):
+        thickness = (3.0 / (4.0 * np.pi)) ** (1.0 / 3.0) * 2.0
+        datapoints = placentagen.uniform_data_on_ellipsoid(3, 1.0, thickness, 1.0, 0)
+        array_test = np.isclose(datapoints[1][:], [0.57526684, -0.14461422, 0.18163017])
+        self.assertTrue(array_test.all)
+
 
 class Test_create_trees(TestCase):
     def test_umilical_node(self):
