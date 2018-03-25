f3d4129ddcbc7d7b5042f7ecef09b7e84fdfd5a7
Alys Clark <alys.clark@auckland.ac.nz>
2018-03-25 19:43:43 +1300

    Adding comments and making use of new functions to minimise repeated code

--- source/placentagen/pg_utilities.py
+++ source/placentagen/pg_utilities.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import numpy as np
 
+
+def calculate_ellipse_radii(volume, thickness, ellipticity):
+    pi = np.pi
+    z_radius = thickness / 2.0
+    x_radius = np.sqrt(volume * 3.0 / (4.0 * pi * ellipticity * z_radius))
+    y_radius = ellipticity * x_radius
+
+    return {'x_radius': x_radius, 'y_radius': y_radius, 'z_radius': z_radius}
+
+
 def z_from_xy(x, y, x_radius, y_radius, z_radius):
     z = z_radius * np.sqrt(1.0 - (x / x_radius) ** 2 - (y / y_radius) ** 2)
     return z
