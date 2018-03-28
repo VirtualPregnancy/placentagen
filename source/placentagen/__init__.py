f81241a9aad0ca97343649d9e5fba0c564b19e9f
wtun899 <wtun899@aucklanduni.ac.nz>
2018-03-28 16:57:48 +1300

    adding terminal_branch subroutine in the file

--- source/placentagen/__init__.py
+++ source/placentagen/__init__.py
@@ -3,3 +3,4 @@ from .generate_shapes import uniform_data_on_ellipsoid
 from .generate_shapes import umbilical_seed_geometry
 from .imports_and_exports import import_exnode_tree
 from .imports_and_exports import import_exelem_tree
+from .Terminal_Br import terminal_branch
