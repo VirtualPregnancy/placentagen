a9bac515ec6d470034b5528848c65424691567c9
wtun899 <wtun899@aucklanduni.ac.nz>
2018-03-29 10:23:09 +1300

    add a line to call meshgridgen subroutine

--- source/placentagen/__init__.py
+++ source/placentagen/__init__.py
@@ -4,3 +4,5 @@ from .generate_shapes import umbilical_seed_geometry
 from .imports_and_exports import import_exnode_tree
 from .imports_and_exports import import_exelem_tree
 from .Terminal_Br import terminal_branch
+from .MESHGRIDGEN import meshgridgen
+
