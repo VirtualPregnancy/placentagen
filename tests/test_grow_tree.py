87df1871157ffe49ccfedf7ff4ff7dc839b42c07
Alys Clark <alys.clark@auckland.ac.nz>
2018-04-02 20:48:04 +1200

    Correcting typos in test

--- tests/test_grow_tree.py
+++ tests/test_grow_tree.py
@@ -21,11 +21,12 @@ class Test_create_trees(TestCase):
 
 class Test_grow_trees(TestCase):
     def test_grow_node(self):
+        seed_geom = {}
         seed_geom['umb_nodes'] = [[0, 0, 1, 0], [1, 0, .1, 0], [2, -0.1, 0.1, 0], [3, 0.1, 0.1, 0]]
         seed_geom['umb_elems'] = [[0, 0, 1], [1, 1, 2], [2, 1, 3]]
         seed_geom['elem_up'] = [[0, 0, 0], [1, 0, 0], [1, 0, 0]]
         seed_geom['elem_down'] = [[2, 1, 2], [0, 0, 0], [0, 0, 0]]
         data = [[-0.1, 0.0, 0.0], [0.1, 0.0, 0.0], [0.0, 0.0, 0.3], [0.1, 0.1, 0.1], [-0.2, -0.2, -0.2]]
-        chorion_geom = pg.grow_chorionic_surface(90 * np.pi / 180, 45 * np.pi / 180, 0.5, 0.1, 1,
+        chorion_geom = placentagen.grow_chorionic_surface(90 * np.pi / 180, 45 * np.pi / 180, 0.5, 0.1, 1,
                                                  1, 1, 1, data, seed_geom, 'surface')
-        self.assertTrue(chorin_geom['nodes'][6][3], 0.48527182)
+        self.assertTrue(chorion_geom['nodes'][6][3], 0.48527182)
