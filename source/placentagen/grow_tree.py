808035c9d5a32a7cd712edcd30b828f4bd6017ed
wtun899 <wtun899@aucklanduni.ac.nz>
2018-04-04 11:06:28 +1200

    removing previoulsy added terminal_branch subroutine

--- source/placentagen/grow_tree.py
+++ source/placentagen/grow_tree.py
@@ -621,18 +621,5 @@ def mesh_com(ld_val, ld, datapoints):
     return com
 
 
-def terminal_branch(Br_El):#Br_El is el_array output from import_exeelem_tree subroutine
 
-    T_Br=np.ones((len(Br_El),1))#if it is 1, it is terminal br, if it is zero it is not, initialise all branches as terminal
-
-    for i in range(0, len(Br_El)):
-        for j in range(0,len(Br_El)):     
-        
-            if Br_El[j][1] == Br_El[i][2]:# if the first node of any elment is equal to 2nd node another element, 
-               T_Br[i] = 0 #the element is not terminal (zero means 'not terminal')
-             
-       
-    Terminal_El_num=np.array(np.where(T_Br[:,0]==1))#looking for the element number which are terminal branch 
-    
-    return {'terminal_el': Terminal_El_num, 'total_terminal_el': len(Terminal_El_num[0])}
 
