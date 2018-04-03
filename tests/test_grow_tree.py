0227882f99aea55a5826a65ef1aaf40584f026d0
wtun899 <wtun899@aucklanduni.ac.nz>
2018-04-04 10:47:55 +1200

    adding unittest for terminal_branch subroutine into existing file

--- tests/test_grow_tree.py
+++ tests/test_grow_tree.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
 import numpy as np
-
+import unittest
 import placentagen
-
+import os
 
 class Test_create_trees(TestCase):
     def test_umilical_node(self):
@@ -30,3 +30,22 @@ class Test_grow_trees(TestCase):
         chorion_geom = placentagen.grow_chorionic_surface(90 * np.pi / 180, 45 * np.pi / 180, 0.5, 0.1, 1,
                                                  1, 1, 1, data, seed_geom, 'surface')
         self.assertTrue(chorion_geom['nodes'][6][3], 0.48527182)
+
+TESTDATA_FILENAME1 = os.path.join(os.path.dirname(__file__), 'Testdata/Small.exelem')
+
+class Test_Terminal_Br(TestCase):
+        
+    def test_terminal_br(self):
+        eldata   = placentagen.import_exelem_tree(TESTDATA_FILENAME1)
+        term_br  = placentagen.terminal_branch(eldata['el_array'])
+        self.assertTrue(np.isclose(term_br['terminal_el'][0,1], 2))
+        
+    def test_terminal_br_total(self):
+        eldata   = placentagen.import_exelem_tree(TESTDATA_FILENAME1)
+        term_br_total  = placentagen.terminal_branch(eldata['el_array'])
+        self.assertTrue(np.isclose(term_br_total['total_terminal_el'], 2))
+
+
+if __name__ == '__main__':
+   unittest.main()
+
