4c69bf02cfd69c41ad2b4630c776c70457b98335
wtun899 <wtun899@aucklanduni.ac.nz>
2018-04-04 11:04:08 +1200

    remove the terminal_branch subroutine from the file

--- tests/test_grow_tree.py
+++ tests/test_grow_tree.py
@@ -31,21 +31,5 @@ class Test_grow_trees(TestCase):
                                                  1, 1, 1, data, seed_geom, 'surface')
         self.assertTrue(chorion_geom['nodes'][6][3], 0.48527182)
 
-TESTDATA_FILENAME1 = os.path.join(os.path.dirname(__file__), 'Testdata/Small.exelem')
 
-class Test_Terminal_Br(TestCase):
-        
-    def test_terminal_br(self):
-        eldata   = placentagen.import_exelem_tree(TESTDATA_FILENAME1)
-        term_br  = placentagen.terminal_branch(eldata['el_array'])
-        self.assertTrue(np.isclose(term_br['terminal_el'][0,1], 2))
-        
-    def test_terminal_br_total(self):
-        eldata   = placentagen.import_exelem_tree(TESTDATA_FILENAME1)
-        term_br_total  = placentagen.terminal_branch(eldata['el_array'])
-        self.assertTrue(np.isclose(term_br_total['total_terminal_el'], 2))
-
-
-if __name__ == '__main__':
-   unittest.main()
 
