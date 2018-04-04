78c4536d31d3d9c048880ae4c065eb6b70a37ea2
wtun899 <wtun899@aucklanduni.ac.nz>
2018-04-04 15:17:14 +1200

    removing error part

--- tests/test_analyse_tree.py
+++ tests/test_analyse_tree.py
@@ -29,19 +29,7 @@ class Test_pos_axis(TestCase):
         
         self.assertTrue(np.isclose(pos_coor['new_node_array'][3][3], 0.2999999999999998))
         
-class Test_term_br_location(TestCase):
-        
-    def test_term_br_loc(self):
-        
-        eldata=placentagen.import_exelem_tree(TESTDATA_FILENAME1)
-        nodedata=placentagen.import_exnode_tree(TESTDATA_FILENAME)
-        term_br=placentagen.terminal_branch(eldata['el_array'])
-        pos_node= placentagen.pos_axis(nodedata['node_array'],2.5,5,3.54)
-        print term_br['terminal_el']
-        print eldata['el_array']
-        print pos_node['new_node_array']
-        term_block=placentagen.terminal_block(term_br['terminal_el'],2,5,4,eldata['el_array'],pos_node['new_node_array'],0,0,0,2.5,2,2)
-        self.assertTrue(np.isclose(term_block['total_term_block'], 2))
+
 
 if __name__ == '__main__':
    unittest.main()
