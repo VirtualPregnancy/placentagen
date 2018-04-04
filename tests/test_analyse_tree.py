d75bbd986de4d58f370b9ceb40efeb7540c4f0f4
wtun899 <wtun899@aucklanduni.ac.nz>
2018-04-04 15:43:03 +1200

    adding unittest

--- tests/test_analyse_tree.py
+++ tests/test_analyse_tree.py
@@ -29,7 +29,17 @@ class Test_pos_axis(TestCase):
         
         self.assertTrue(np.isclose(pos_coor['new_node_array'][3][3], 0.2999999999999998))
         
-
+class Test_term_br_location(TestCase):
+        
+    def test_term_br_loc(self):
+        
+        eldata=import_exelem_tree(TESTDATA_FILENAME1)
+        nodedata=import_exnode_tree(TESTDATA_FILENAME)
+        term_br=terminal_branch(eldata['el_array'])
+        pos_node= pos_axis(nodedata['node_array'],2.5,5,3.54)
+        term_block=terminal_block(term_br['terminal_el'],nel_x,nel_y,nel_z,eldata['el_array'],pos_node['new_node_array'],x_min,y_min,z_min,x_width,y_width,z_width)
+        self.assertTrue(np.isclose(term_block['total_term_block'], 2))
+        
 
 if __name__ == '__main__':
    unittest.main()
