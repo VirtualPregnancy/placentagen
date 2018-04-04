b38803ff251fee575dc06191fda14992dd53e22c
wtun899 <wtun899@aucklanduni.ac.nz>
2018-04-04 12:11:06 +1200

    adding gen_rectangular_mesh subroutine code in existing file

--- source/placentagen/generate_shapes.py
+++ source/placentagen/generate_shapes.py
@@ -102,4 +102,37 @@ def uniform_data_on_ellipsoid(n, volume, thickness, ellipticity, random_seed):
 
     return chorion_data
 
+def gen_rectangular_mesh(x_min,x_max,y_min,y_max,z_min,z_max,nel_x,nel_y,nel_z,x_width,y_width,z_width):
+    x = np.linspace(x_min,x_max, x_max/x_width+1)#linspace for x axis
+    y = np.linspace(y_min, y_max, y_max/y_width+1)#linspace for y axis
+    z = np.linspace(z_min, z_max, z_max/z_width+1)#linspace for z axis
+    nodes = np.vstack(np.meshgrid(y,z,x)).reshape(3,-1)#generate nodes for rectangular mesh
 
+    y=np.array(nodes[0,:])#y coordinates of each node
+    z=np.array(nodes[1,:])#z coordinates of each node
+    x=np.array(nodes[2,:])#x coordinates of each node
+
+    #Generating the element connectivity of each cube element, 8 nodes for each 3D cube element
+    nodeOfelement=np.zeros((8,nel_x*nel_y*nel_z))#this stores the nodes of each mesh element
+    E1=0
+
+    for K1 in range (1,nel_z+1):
+       for J1 in range (1,nel_y+1):
+          for I1 in range(1,nel_x+1):
+           
+            nodeOfelement[0,E1] = I1+(nel_x+1)*(J1-1)+(nel_x+1)*(nel_y+1)*(K1-1)#1st node
+            nodeOfelement[1,E1] = nodeOfelement[0,E1]+1#2nd node
+            nodeOfelement[2,E1] = nodeOfelement[0,E1]+nel_x+1#3rd node
+            nodeOfelement[3,E1] = nodeOfelement[2,E1]+1#4th node
+            nodeOfelement[4,E1] = nodeOfelement[0,E1]+(nel_x+1)*(nel_y+1)#5th node
+            nodeOfelement[5,E1] = nodeOfelement[1,E1]+(nel_x+1)*(nel_y+1)#6th node
+            nodeOfelement[6,E1] = nodeOfelement[2,E1]+(nel_x+1)*(nel_y+1)#7th node
+            nodeOfelement[7,E1] = nodeOfelement[3,E1]+(nel_x+1)*(nel_y+1)#8th node
+            
+            E1 = E1+1;
+
+    nodeOfelement=nodeOfelement.T
+    
+
+
+    return {'nodeOfelement': nodeOfelement, 'x_coor': x,'y_coor':y,'z_coor':z,'total_mesh_el':nel_x*nel_y*nel_z,'total_mesh_node':(nel_x+1)*(nel_y+1)*(nel_z+1)}
