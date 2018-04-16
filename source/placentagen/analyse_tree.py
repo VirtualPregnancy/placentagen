bc48bf202d54d9bb132b84c55857d0b0235e9e91
Alys Clark <alys.clark@auckland.ac.nz>
2018-04-16 22:17:06 +1200

    New algorithm for assessing volume of ellipsoid in regular rectangular grid

--- source/placentagen/analyse_tree.py
+++ source/placentagen/analyse_tree.py
@@ -2,48 +2,49 @@
 import numpy as np
 from . import pg_utilities
 
-def calc_terminal_branch(node_loc,elems):
-    #This function generates a list of terminal nodes associated with a branching geometry
-    #inputs are node locations and elements
+
+def calc_terminal_branch(node_loc, elems):
+    # This function generates a list of terminal nodes associated with a branching geometry
+    # inputs are node locations and elements
     num_elems = len(elems)
     num_nodes = len(node_loc)
     elem_cnct = pg_utilities.element_connectivity_1D(node_loc, elems)
 
-    terminal_branches = np.zeros(num_elems, dtype = int)
-    terminal_nodes = np.zeros(num_nodes, dtype = int)
+    terminal_branches = np.zeros(num_elems, dtype=int)
+    terminal_nodes = np.zeros(num_nodes, dtype=int)
 
     num_term = 0
-    for ne in range(0,num_elems):
-        if elem_cnct['elem_down'][ne][0] == 0: #no downstream element
+    for ne in range(0, num_elems):
+        if elem_cnct['elem_down'][ne][0] == 0:  # no downstream element
             terminal_branches[num_term] = ne
-            terminal_nodes[num_term] = elems[ne][2] #node that leaves the terminal element
+            terminal_nodes[num_term] = elems[ne][2]  # node that leaves the terminal element
             num_term = num_term + 1
 
-    terminal_branches = np.resize(terminal_branches,num_term)
-    terminal_nodes = np.resize(terminal_nodes,num_term)
+    terminal_branches = np.resize(terminal_branches, num_term)
+    terminal_nodes = np.resize(terminal_nodes, num_term)
 
     print('Total number of terminals assessed, num_terminals =  ' + str(num_term))
 
     return {'terminal_elems': terminal_branches, 'terminal_nodes': terminal_nodes, 'total_terminals': num_term}
 
 
-def terminals_in_sampling_grid(rectangular_mesh,terminal_list,node_loc):
-    #This function counts the number of terminals in a sampling grid element
-    #inputs are:
-    #Rectangular mesh - the sampling grid
-    #terminal_list - a list of terminals
-    #node_loc - location of nodes
+def terminals_in_sampling_grid(rectangular_mesh, terminal_list, node_loc):
+    # This function counts the number of terminals in a sampling grid element
+    # inputs are:
+    # Rectangular mesh - the sampling grid
+    # terminal_list - a list of terminals
+    # node_loc - location of nodes
     num_sample_elems = rectangular_mesh['total_elems']
     num_terminals = terminal_list['total_terminals']
-    terminals_in_grid = np.zeros(num_sample_elems,dtype = int)
+    terminals_in_grid = np.zeros(num_sample_elems, dtype=int)
 
-    for ne in range(0,num_sample_elems):
-        #First node has min x,y,z and last node has max x,y,z
+    for ne in range(0, num_sample_elems):
+        # First node has min x,y,z and last node has max x,y,z
         first_node = rectangular_mesh['elems'][ne][1]
         last_node = rectangular_mesh['elems'][ne][8]
         min_coords = rectangular_mesh['nodes'][first_node][0:3]
         max_coords = rectangular_mesh['nodes'][last_node][0:3]
-        for nt in range(0,num_terminals):
+        for nt in range(0, num_terminals):
             in_element = [False, False, False]
             coord_terminal = node_loc[terminal_list['terminal_nodes'][nt]][1:4]
             if coord_terminal[0] >= min_coords[0] and coord_terminal[0] < max_coords[0]:
@@ -52,12 +53,13 @@ def terminals_in_sampling_grid(rectangular_mesh,terminal_list,node_loc):
                     in_element[1] = True
                     if coord_terminal[2] >= min_coords[2] and coord_terminal[2] < max_coords[2]:
                         in_element[2] = True
-            if(np.all(in_element)):
-                terminals_in_grid[ne] = terminals_in_grid[ne]+1
+            if (np.all(in_element)):
+                terminals_in_grid[ne] = terminals_in_grid[ne] + 1
 
     return terminals_in_grid
 
-def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity,num_test_points,method):
+
+def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity, num_test_points, method):
     # This subroutine calculates the placental volume associated with each element in a samplling grid
     # inputs are:
     # rectangular_mesh = the sampling grid nodes and elements
@@ -76,7 +78,6 @@ def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity,num_
     pl_vol_in_grid = np.zeros(
         total_elems)  # 1st col stores the number of samp_grid_el, 2nd col stores 0/1/2 (depend on type of samp_grid_el), 3rd col stores the pl_vol in that sam_grid_el
 
-
     xVector = np.linspace(0.0, 1.0, num_test_points)
     yVector = np.linspace(0.0, 1.0, num_test_points)
     zVector = np.linspace(0.0, 1.0, num_test_points)
@@ -86,6 +87,12 @@ def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity,num_
     print(calculating_nodes[0][2])
 
     for ne in range(0, len(elems)):  # looping through elements
+        startx = nodes[elems[ne][1]][0]
+        endx = nodes[elems[ne][8]][0]
+        starty = nodes[elems[ne][1]][1]
+        endy = nodes[elems[ne][8]][1]
+        startz = nodes[elems[ne][1]][2]
+        endz = nodes[elems[ne][8]][2]
         count_in_range = 0
         nod_in_range = np.zeros(8, dtype=int)
         for nod in range(1, 9):
@@ -97,59 +104,76 @@ def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity,num_
                 count_in_range = count_in_range + 1
                 nod_in_range[nod - 1] = 1
         if count_in_range == 8:  # if all 8 nodes are inside the ellipsoid
-            pl_vol_in_grid[ne] = 1.0  # the placental vol in that samp_grid_el is same as vol of samp_grid_el
+            pl_vol_in_grid[ne] = (endx - startx) * (endy - starty) * (endz - startz)  # the placental vol in that samp_grid_el is same as vol of samp_grid_el
         elif count_in_range == 0:  # if all 8 nodes are outside the ellpsiod
-            pl_vol_in_grid[ne] = 0  # since this samp_grid_el is completely outside, the placental vol is zero (there will be no tree here and no need to worried about the vol)
+            pl_vol_in_grid[
+                ne] = 0  # since this samp_grid_el is completely outside, the placental vol is zero (there will be no tree here and no need to worried about the vol)
         else:  # if some nodes in and some nodes out, the samp_grid_el is at the edge of ellipsoid
-            startx = nodes[elems[ne][1]][0]
-            endx = nodes[elems[ne][8]][0]
-            starty = nodes[elems[ne][1]][1]
-            endy = nodes[elems[ne][8]][1]
-            startz = nodes[elems[ne][1]][2]
-            endz = nodes[elems[ne][8]][2]
-            if(method == 'summing'):
+            if (method == 'summing'):
                 pointcount = 0
                 for nnod in range(0, len(calculating_nodes)):
-                    x = calculating_nodes[nnod][0]*(endx-startx)+startx
-                    y = calculating_nodes[nnod][1]*(endy-starty)+starty
-                    z = calculating_nodes[nnod][2]*(endz-startz)+startz
-                    point_check = pg_utilities.check_in_ellipsoid(x,y,z, x_radius, y_radius, z_radius)
+                    x = calculating_nodes[nnod][0] * (endx - startx) + startx
+                    y = calculating_nodes[nnod][1] * (endy - starty) + starty
+                    z = calculating_nodes[nnod][2] * (endz - startz) + startz
+                    point_check = pg_utilities.check_in_ellipsoid(x, y, z, x_radius, y_radius, z_radius)
                     if point_check:  # if the point fall inside the ellipsoid
-                        pointcount = pointcount+1
-                    pl_vol_in_grid[ne] = float(pointcount)/float(num_test_points*num_test_points*num_test_points)
-            else: #Use quadrature
-                #need to map to positive quadrant
-                if (startx >=0 and starty >=0 and startz >=0):
+                        pointcount = pointcount + 1
+                    pl_vol_in_grid[ne] = float(pointcount) / float(num_test_points * num_test_points * num_test_points)
+            else:  # Use quadrature
+                # need to map to positive quadrant
+                repeat = False
+                if (startz < 0 and endz <=0):
+                    #need to project to positive z axis
+                    startz = abs(nodes[elems[ne][8]][2])
+                    endz = abs(nodes[elems[ne][1]][2])
+                elif(startz < 0 and endz > 0):
+                    #Need to split into components above and below the axis and sum the two
+                    startz = 0
+                    endz = abs(nodes[elems[ne][1]][2])
+                    startz_2=0
+                    endz_2 =nodes[elems[ne][8]][2]
+                    repeat = True
+                xVector = np.linspace(startx, endx, num_test_points)
+                yVector = np.linspace(starty, endy, num_test_points)
+                xv, yv = np.meshgrid(xVector, yVector)
+                zv = z_radius ** 2 * (1 - (xv / x_radius) ** 2 - (yv / y_radius) ** 2)
+                for i in range(num_test_points):
+                    for j in range(num_test_points):
+                        if zv[i, j] <= startz ** 2:
+                            zv[i, j] = startz ** 2
+                        zv[i, j] = np.sqrt(zv[i, j])
+                        if zv[i, j] > endz:
+                            zv[i, j] = endz
+                        elif zv[i, j] < startz:
+                            zv[i, j] = startz
+                intermediate = np.zeros(num_test_points)
+                for i in range(0, num_test_points):
+                    intermediate[i] = np.trapz(zv[:, i], xVector)
+                Value1 = np.trapz(intermediate, yVector)
+                pl_vol_in_grid[ne] = (Value1 - startz * (endx - startx) * (endy - starty))# / ((endx - startx) * (endy - starty) * (endz - startz))
+                if repeat:
                     xVector = np.linspace(startx, endx, num_test_points)
                     yVector = np.linspace(starty, endy, num_test_points)
-                    xv,yv = np.meshgrid(xVector,yVector,indexing='ij')
-                    zv = z_radius**2 * (1-(xv/x_radius)**2 -(yv/y_radius)**2)
-                    zv=zv[zv > startz**2]
-                    zv = np.sqrt(zv)
-                    z1 = zv > endz
-                    z2 = zv < endz
-                    z3 = zv < startz
-                    zv = z1*endz + z2*zv + z3*startz
-
-                    ## do a 1-D integral over every row
-                    ## -----------------------------------
-                    #I = np.zeros(Ny)
-                    #for i in range(Ny):
-                    #    I[i] = np.trapz(y[i, :], xlin)
-
-                    #Value1 = np.trapz(xv,x=zv,axis = 2)
-                    #for i in range(num_test_points):
-                    #    for j in range(num_test_points):
-                    #        if zv[i][j] < startz:
-                    #            zv[i][j] = 0
-                    #        else:
-                    #            zv[i][j] = np.sqrt(zv[i,j])
-                    print(zv),Value1
-
-
+                    xv, yv = np.meshgrid(xVector, yVector)
+                    zv = z_radius ** 2 * (1 - (xv / x_radius) ** 2 - (yv / y_radius) ** 2)
+                    for i in range(num_test_points):
+                        for j in range(num_test_points):
+                            if zv[i, j] <= startz_2 ** 2:
+                                zv[i, j] = startz_2 ** 2
+                            zv[i, j] = np.sqrt(zv[i, j])
+                            if zv[i, j] > endz_2:
+                                zv[i, j] = endz_2
+                            elif zv[i, j] < startz_2:
+                                zv[i, j] = startz_2
+                    intermediate = np.zeros(num_test_points)
+                    for i in range(0, num_test_points):
+                        intermediate[i] = np.trapz(zv[:, i], xVector)
+                    Value1 = np.trapz(intermediate, yVector)
+                    pl_vol_in_grid[ne] =pl_vol_in_grid[ne] + (Value1 - startz_2 * (endx - startx) * (
+                                endy - starty))  # / ((endx - startx) * (endy - starty) * (endz - startz))
 
 
     print(pl_vol_in_grid)
+    print(np.sum(pl_vol_in_grid))
 
     return pl_vol_in_grid
-
