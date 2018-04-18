1576aa07c7665ab55da728ef5e1c619a194c49a9
Alys Clark <alys.clark@auckland.ac.nz>
2018-04-18 20:16:29 +1200

    Some speed ups for adding data points to cube elements

--- source/placentagen/analyse_tree.py
+++ source/placentagen/analyse_tree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 from . import pg_utilities
+import time
 
 
 def calc_terminal_branch(node_loc, elems):
@@ -28,34 +29,47 @@ def calc_terminal_branch(node_loc, elems):
     return {'terminal_elems': terminal_branches, 'terminal_nodes': terminal_nodes, 'total_terminals': num_term}
 
 
-def terminals_in_sampling_grid(rectangular_mesh, terminal_list, node_loc):
+def terminals_in_sampling_grid(rectangular_mesh, placenta_list, terminal_list, node_loc):
     # This function counts the number of terminals in a sampling grid element
     # inputs are:
     # Rectangular mesh - the sampling grid
-    # terminal_list - a list of terminals
+    # terminal_list - a list of terminal,
     # node_loc - location of nodes
-    num_sample_elems = rectangular_mesh['total_elems']
+    num_sample_elems = len(placenta_list)
     num_terminals = terminal_list['total_terminals']
-    terminals_in_grid = np.zeros(num_sample_elems, dtype=int)
+    terminals_in_grid = np.zeros(len(rectangular_mesh['elems']), dtype=int)
+    terminal_mapped = np.zeros(num_terminals, dtype=int)
 
-    for ne in range(0, num_sample_elems):
+
+    print('length of data', len(placenta_list))
+
+    for ne_i in range(0, num_sample_elems):
         # First node has min x,y,z and last node has max x,y,z
-        first_node = rectangular_mesh['elems'][ne][1]
-        last_node = rectangular_mesh['elems'][ne][8]
-        min_coords = rectangular_mesh['nodes'][first_node][0:3]
-        max_coords = rectangular_mesh['nodes'][last_node][0:3]
-        for nt in range(0, num_terminals):
-            in_element = [False, False, False]
-            coord_terminal = node_loc[terminal_list['terminal_nodes'][nt]][1:4]
-            if coord_terminal[0] >= min_coords[0] and coord_terminal[0] < max_coords[0]:
-                in_element[0] = True
-                if coord_terminal[1] >= min_coords[1] and coord_terminal[1] < max_coords[1]:
-                    in_element[1] = True
-                    if coord_terminal[2] >= min_coords[2] and coord_terminal[2] < max_coords[2]:
-                        in_element[2] = True
-            if (np.all(in_element)):
-                terminals_in_grid[ne] = terminals_in_grid[ne] + 1
-
+        ne = placenta_list[ne_i]
+        start = time.time()
+        if placenta_list[ne_i] > 0: #There is some placenta in this element (assuming none in el 0)
+            first_node = rectangular_mesh['elems'][ne][1]
+            last_node = rectangular_mesh['elems'][ne][8]
+            min_coords = rectangular_mesh['nodes'][first_node][0:3]
+            max_coords = rectangular_mesh['nodes'][last_node][0:3]
+            for nt in range(0, num_terminals):
+                if terminal_mapped[nt] == 0:
+                    in_element = False
+                    coord_terminal = node_loc[terminal_list['terminal_nodes'][nt]][1:4]
+                    if coord_terminal[0] >= min_coords[0]:
+                        if coord_terminal[0] < max_coords[0]:
+                            if coord_terminal[1] >= min_coords[1]:
+                                if coord_terminal[1] < max_coords[1]:
+                                    if coord_terminal[2] >= min_coords[2]:
+                                        if coord_terminal[2] < max_coords[2]:
+                                            in_element = True
+                    if in_element:
+                        print(nt, in_element)
+                        terminals_in_grid[ne] = terminals_in_grid[ne] + 1
+                        terminal_mapped[nt] = 1
+        end = time.time()
+        print('Time taken for this element: ' + str(ne))
+        print(end-start, np.sum(terminal_mapped))
     return terminals_in_grid
 
 
@@ -77,6 +91,8 @@ def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity, num
 
     #Initialise the array that defines the volume of placenta in each grid element
     pl_vol_in_grid = np.zeros(total_elems)
+    non_empty_loc = np.zeros(total_elems, dtype = int)
+    non_empty_count = 0
 
     for ne in range(0, len(elems)):  # looping through elements
         count_in_range = 0
@@ -97,12 +113,16 @@ def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity, num
                 count_in_range = count_in_range + 1
                 nod_in_range[nod - 1] = 1
         if count_in_range == 8:  # if all 8 nodes are inside the ellipsoid
+            non_empty_loc[non_empty_count] = ne
+            non_empty_count = non_empty_count + 1
             pl_vol_in_grid[ne] = (endx - startx) * (endy - starty) * (endz - startz)  # the placental vol in that samp_grid_el is same as vol of samp_grid_el
         elif count_in_range == 0:  # if all 8 nodes are outside the ellpsiod
-            pl_vol_in_grid[
-                ne] = 0  # since this samp_grid_el is completely outside, the placental vol is zero (there will be no tree here and no need to worried about the vol)
+            # since this samp_grid_el is completely outside, the placental vol is zero
+            pl_vol_in_grid[ne] = 0
         else:  # if some nodes in and some nodes out, the samp_grid_el is at the edge of ellipsoid
         #Use trapezoidal quadrature to caculate the volume under the surface of the ellipsoid in each element
+            non_empty_loc[non_empty_count] = ne
+            non_empty_count = non_empty_count + 1
             # need to map to positive quadrant
             repeat = False
             if (startz < 0 and endz <=0):
@@ -155,4 +175,8 @@ def ellipse_volume_to_grid(rectangular_mesh, volume, thickness, ellipticity, num
                 pl_vol_in_grid[ne] =pl_vol_in_grid[ne] + (Value1 - startz_2 * (endx - startx) * (
                                 endy - starty))
 
-    return pl_vol_in_grid
+    print('Number of Non-empty cells: ' + str(non_empty_count))
+    print('Total number of cells: ' + str(total_elems))
+    non_empty_loc = np.resize(non_empty_loc, non_empty_count)
+
+    return {'pl_vol_in_grid': pl_vol_in_grid, 'non_empty_rects': non_empty_loc}
