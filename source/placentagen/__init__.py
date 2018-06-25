02fc66f750bcecf3b3da535d7e7ac46a28376606
Win <wtun899@aucklanduni.ac.nz>
2018-06-26 08:44:42 +1200

    debugging

--- source/placentagen/__init__.py
+++ source/placentagen/__init__.py
@@ -2,4 +2,4 @@ from .generate_shapes import *
 from .imports_and_exports import *
 from .grow_tree import *
 from .analyse_tree import *
-from scipy.spatial import Delaunay
+from .scipy.spatial import *
